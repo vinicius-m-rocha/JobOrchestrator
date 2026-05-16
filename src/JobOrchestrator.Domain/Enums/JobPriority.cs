b36e66cf1ba7c11eff6c79c9472586ad31094f18
Vinicius Rocha <vinicius.rocha2016@hotmail.com>
2026-05-16 19:21:50 -0300

    feat: Create entity job structure

---
+++ src/JobOrchestrator.Domain/Enums/JobPriority.cs
@@ -0,0 +1,8 @@
+﻿namespace JobOrchestrator.Domain.Enums;
+
+public enum JobPriority
+{
+    Low = 0,
+    Normal = 1,
+    High = 2
+}
---
+++ src/JobOrchestrator.Domain/ValuesObjects/ExecutionAttempt.cs
@@ -0,0 +1,12 @@
+﻿namespace JobOrchestrator.Domain.ValuesObjects;
+
+public class ExecutionAttempt(
+    int attemptNumber,
+    bool isSuccess,
+    string? errorMessage = null)
+{
+    public int Attemptnumber { get; } = attemptNumber;
+    public bool IsSuccess { get; } = isSuccess;
+    public string? ErrorMessage { get; } = errorMessage;
+    public DateTime ExecutedAt { get; } = DateTime.UtcNow;
+}
