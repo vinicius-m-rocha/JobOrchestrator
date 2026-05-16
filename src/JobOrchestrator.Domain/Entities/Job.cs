884debb77e004a0502fd5286812a515b977f036f
Vinicius Rocha <vinicius.rocha2016@hotmail.com>
2026-05-16 19:20:42 -0300

    feat: Create entity job structure

---
+++ src/JobOrchestrator.Domain/Entities/Job.cs
@@ -0,0 +1,23 @@
+﻿using JobOrchestrator.Domain.Enums;
+using JobOrchestrator.Domain.ValuesObjects;
+
+namespace JobOrchestrator.Domain.Entities;
+
+public class Job(
+    string idempotencyKey,
+    JobPriority priority,
+    string payload,
+    DateTime scheduledAt,
+    JobStatus status = JobStatus.Pending)
+{
+    public string Id { get; private set; } = Guid.CreateVersion7().ToString();
+    public string IdempotencyKey { get; private set; } = idempotencyKey;
+    public JobPriority Priority { get; private set; } = priority;
+    public string Payload { get; private set; } = payload;
+    public DateTime ScheduledAt { get; private set; } = scheduledAt;
+    public JobStatus Status { get; private set; } = status;
+
+
+    private readonly List<ExecutionAttempt> _executionAttempts = [];
+    public IReadOnlyCollection<ExecutionAttempt> ExecutionAttempts => _executionAttempts.AsReadOnly();
+}
