91cbdb9611b76c25927153efc39bf09199a09f78
Vinicius Rocha <vinicius.rocha2016@hotmail.com>
2026-05-17 12:12:52 -0300

    feat: Create Command to insert Job

---
+++ src/JobOrchestrator.Application/Features/Jobs/CreateJob/CreateJobCommand.cs
@@ -0,0 +1,10 @@
+﻿using JobOrchestrator.Domain.Enums;
+using MediatR;
+
+namespace JobOrchestrator.Application.Features.Jobs.CreateJob;
+
+public record CreateJobCommand(
+    string IdempotencyKey,
+    JobPriority Priority,
+    string Payload,
+    DateTime? ScheduledAt) : IRequest<string>;
---
+++ src/JobOrchestrator.Application/Features/Jobs/CreateJob/CreateJobValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace JobOrchestrator.Application.Features.Jobs.CreateJob;
+
+public class CreateJobValidator : AbstractValidator<CreateJobCommand>
+{
+    public CreateJobValidator()
+    {
+        RuleFor(x => x.IdempotencyKey)
+            .NotEmpty().WithMessage("IdempotencyKey is required.")
+            .MaximumLength(100).WithMessage("IdempotencyKey must not exceed 100 characters.");
+
+        RuleFor(x => x.Priority)
+            .IsInEnum().WithMessage("Invalid job priority.");
+
+        RuleFor(x => x.Payload)
+            .NotEmpty().WithMessage("Payload cannot be empty.");
+
+        RuleFor(x => x.ScheduledAt)
+            .GreaterThan(DateTime.UtcNow).When(x => x.ScheduledAt.HasValue)
+            .WithMessage("Scheduled time must be in the future.");
+    }
+}
