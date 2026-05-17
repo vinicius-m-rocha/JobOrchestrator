91976015d73515b2de3ea41210a316423e1ff0e4
Vinicius Rocha <vinicius.rocha2016@hotmail.com>
2026-05-16 23:17:13 -0300

    feat: Configure mediator and pipeline beahivor

---
+++ src/JobOrchestrator.Application/AddApplicationCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using JobOrchestrator.Application.Behaviors;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JobOrchestrator.Application;
+
+public static class AddApplicationCollectionExtensions
+{
+    public static IServiceCollection AddApplicationService(this IServiceCollection services)
+    {
+        var assembly = typeof(AddApplicationCollectionExtensions).Assembly;
+
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(assembly);
+
+            cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+        });
+
+        services.AddValidatorsFromAssembly(assembly);
+
+        return services;
+    }
+}
